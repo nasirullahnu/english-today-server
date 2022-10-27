75f26cc674b6f33bc64b885890081c21d6672d82
nasirullahnu <nasirullah878@gmail.com>
2022-10-27 16:33:11 +0600

    server created and data loaded to the client server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/course.json')
+
+
+app.get('/', (req, res) => {
+    res.send('course is running');
+});
+
+app.get('/courses', (req, res)=> {
+    res.send(courses)
+})
+
+app.listen(port, () => {
+    console.log('course running on port', port)
+})
