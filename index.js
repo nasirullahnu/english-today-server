b30a000d6432b8668389e7f97fb96548dc0db105
nasirullahnu <nasirullah878@gmail.com>
2022-10-27 19:38:50 +0600

    json api create and send to the client site

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/category.json')
-const courseDetails = require('./data/course.json')
+const courseDetails = require('./data/course2.json')
 
 
 app.get('/', (req, res) => {
     res.send('course is running');
 });
 
-app.get('/courses', (req, res)=> {
+app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courseDetails)
+    }
+    else {
+        const category_course = courseDetails.filter(cd => cd.category_id === id);
+        res.send(category_course)
+    }
+
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id
+    const selectedcourse = courseDetails.find(course => course._id === id)
+    res.send(selectedcourse)
+})
 
 
 // app.get('/details/:id', (req, res)=>{
